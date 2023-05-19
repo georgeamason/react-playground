bfe89b90f53fbb9cb0d6ad2001d8a605422f5630
George Mason <george.mason@red-gate.com>
2023-05-19 15:01:21 +0100

    Basic table

---
+++ alert-inbox-lite/src/data/alerts.ts
@@ -0,0 +1,18 @@
+const alertData = [
+  {
+    id: '1',
+    type: 'Low disk space',
+    source: 'localhost',
+    status: 'Active',
+    lastUpdated: '15:18'
+  },
+  {
+    id: '2',
+    type: 'Blocking process',
+    source: 'localhost',
+    status: 'Active',
+    lastUpdated: '15:18'
+  }
+]
+
+export default alertData
